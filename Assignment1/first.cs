92f8677277f031870d7bd6ea25de6070976b5895
Aiswaryams3001 <aiswaryams3001@gmail.com>
2022-12-07 09:30:30 +0530

    Add 2 files

---
+++ Assignment1/first.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class first
+    {
+        static void Main(string[] args)
+        {
+            int n, r, temp, sum = 0;
+            Console.WriteLine("Enter a Number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            temp = n;
+            while (temp > 0)
+            {
+                r = n % 10;
+                sum = sum + r;
+                temp = temp / 10;
+
+            }
+            Console.WriteLine("The sum of digits is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment1/ten.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class ten
+    {
+        static void Main(string[] args)
+        {
+            int n, product;
+            Console.WriteLine("Enter a Number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Multiplication Table");
+            for (int i = 1; i <= 10; i++)
+            {
+                product = n * i;
+                Console.WriteLine("{0}*{1}={2}", i, n, product);
+            }
+            Console.ReadLine();
+        }
+    }
+}
